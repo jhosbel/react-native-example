05797412c0bbb907f20e3b7aabb1d2884004f1ae
jhosbel <jhosbel.vargas@gmail.com>
2024-05-04 00:26:03 -0500

    Revert "add the get my location point on google maps"
    This reverts commit b4914cb662de05df761ebdb114367caaa9513613.

--- app/(home)/home.tsx
+++ app/(home)/home.tsx
@@ -1,15 +1,11 @@
 import { View, Text, StyleSheet, Image, TextInput } from "react-native";
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
-import * as Location from "expo-location";
 import BottomSheet, {
   BottomSheetView,
   BottomSheetTextInput,
 } from "@gorhom/bottom-sheet";
 import HomeMapInput from "../../components/HomeMapInput";
-import { Entypo } from "@expo/vector-icons";
-import { FontAwesome } from "@expo/vector-icons";
-import { Ionicons } from "@expo/vector-icons";
 
 const INITIAL_REGION = {
   latitude: 6.167754,
@@ -46,47 +42,27 @@ const Home = () => {
     setText(text);
   };
 
-  let texto = "Waiting..";
-  if (errorMsg) {
-    texto = errorMsg;
-  } else if (location) {
-    texto = JSON.stringify(location);
-  }
-
   return (
     <View style={{ height: "100%" }}>
-      <View className="flex-1 items-center">
+      <View className="flex-1">
         <MapView
           style={StyleSheet.absoluteFill}
           provider={PROVIDER_GOOGLE}
           initialRegion={INITIAL_REGION}
-          showsUserLocation={true}
-          showsMyLocationButton={true}
-          //showsTraffic={true}
-        />
-        <View className="w-[340px] h-[30px] rounded-[5px] shadow border-[#EDEDF6] border bg-white mt-8 flex-row">
-          <View className="h-[7px] w-[7px] bg-[#43B05C] rounded-full self-center ml-[15px]"></View>
-          <Text className="text-[#343B71] self-center ml-[15px]">|</Text>
-          <Text className="text-[#343B71] self-center ml-[15px]">
-            Mi dirección actual
-          </Text>
-          <Entypo name="plus" size={24} color="#343B71" />
-        </View>
-        <View className="w-[340px] h-[30px] rounded-[5px] shadow border-[#EDEDF6] border bg-white mt-8 flex-row">
-          <Text className="text-[#343B71] self-center ml-[15px]">{text}</Text>
-          <Ionicons name="remove" size={24} color="#343B71" />
-        </View>
-        <TextInput
-          placeholder="Prueba"
-          className="w-[340px] h-[30px] rounded-[5px] shadow border-[#EDEDF6] border bg-white mt-8 text-[#343B71]"
+          showsUserLocation
+          showsMyLocationButton
         />
       </View>
       <BottomSheet
         ref={sheetRef}
         snapPoints={snapPoints}
         backgroundStyle={{ backgroundColor: "#343B71" }}
-        enablePanDownToClose={true}
       >
+        {/* <BottomSheetTextInput
+          className="w-[343px] h-[40px] border self-center rounded-[5px] border-[#545C9B] bg-[#282F62] text-white pl-4"
+          placeholder="¿A dónde quieres ir?"
+          placeholderTextColor={"white"}
+        /> */}
         <TextInput
           className="w-[343px] h-[40px] border self-center rounded-[5px] border-[#545C9B] bg-[#282F62] text-white pl-4"
           placeholder="¿A dónde quieres ir?"
